44a9a89d1fa5245c83bc3d39cb7e724f47d85c64
Eddie Burris <burrise@umkc.edu>
2016-11-16 13:36:41 -0600

    add comment for deprecated class

--- TDDExample/app/src/androidTest/java/edu/umkc/burrise/tddexample/ApplicationTest.java
+++ TDDExample/app/src/androidTest/java/edu/umkc/burrise/tddexample/ApplicationTest.java
@@ -6,6 +6,7 @@ import android.test.ApplicationTestCase;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+// ApplicationTestCase was deprecated in API level 24.
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
